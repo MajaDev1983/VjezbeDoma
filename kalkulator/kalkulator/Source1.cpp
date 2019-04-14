3044dd9f7b673e12113bba97ffa7ebc29798f312
MajaDev <majastipetic22@gmail.com>
2019-04-14 16:30:05 +0200

    ignorefajle

---
+++ kalkulator/kalkulator/Source1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int fibonacci(int a)
+{
+	if (a == 0 || a == 1)
+	{
+		return a;
+	}
+	return (fibonacci(a - 1) + fibonacci(a - 2));
+}
+
+
+
+int main()
+{
+	// zadatak pomoæu rekurzije napraviti program koji æe ispisati prvih 20 brojeva fibonnacijevog niza
+
+
+	for (size_t i = 0; i < 20; i++)
+	{
+		std::cout << " " << fibonacci(i);
+	};
+}
